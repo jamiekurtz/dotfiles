3ed717076ba3764fc05bd6a7ae7f72ef6b5b0b46
Jamie Kurtz <jamie@myfirm360.com>
2026-08-29 22:42:53 -0400

    add some more installs

--- setup/new-sway-packages.sh
+++ setup/new-sway-packages.sh
@@ -16,7 +16,7 @@ sudo apt install sway swayidle swaylock swaybg foot \
   zip unzip tree
 
 # other stuff
-sudo apt install -y pipx ranger git-flow golang bash-completion gpg
+sudo apt install -y pipx ranger git-flow golang bash-completion gpg make
 
 # neovim and related
 sudo apt remove neovim
@@ -56,3 +56,83 @@ sudo apt update && sudo apt install 1password
 
 # install claude code
 curl -fsSL https://claude.ai/install.sh | bash
+
+# install AWS CLI
+pushd ~/Downloads
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+popd
+
+# install AWS VPN Client
+curl https://d20adtppz83p9s.cloudfront.net/GTK/latest/awsvpnclient_amd64.deb -o ~/Downloads/awsvpnclient_amd64.debawsvpnclient_amd64.deb
+sudo dpkg -i ~/Downloads/awsvpnclient_amd64.deb
+sudo apt install systemd-resolved
+sudo systemctl --now enable systemd-resolved.service
+sudo systemctl --now enable awsvpnclient
+
+# *****************************
+# install docker engine and docker compose
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update && apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+# *****************************
+# *****  install nodejs  ******
+
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.7/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 24
+
+# Verify the Node.js version:
+node -v # Should print "v24.20.0".
+
+# Verify npm version:
+npm -v # Should print "11.19.0".
+
+# *****************************
+# *****  install zulu 21  ******
+
+sudo apt install -y gnupg ca-certificates curl
+
+curl -s https://repos.azul.com/azul-repo.key |
+  sudo gpg --dearmor -o /usr/share/keyrings/azul.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" |
+  sudo tee /etc/apt/sources.list.d/zulu.list
+
+sudo chmod 644 /usr/share/keyrings/azul.gpg
+
+sudo apt update
+sudo apt install -y zulu21-jdk
+
+ln -sf /usr/lib/jvm/zulu21-ca-amd64 /usr/lib/jvm/default
+
+# install dbeaver
+wget -P ~/Downloads https://dbeaver.io/files/dbeaver-ce-latest-linux-x86_64.deb
+sudo apt install ~/Downloads/dbeaver-ce-26.1.5-linux-x86_64.deb
